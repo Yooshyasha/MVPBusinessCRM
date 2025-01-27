f2a8dcafff847009e76eea043d589481a4c58f5f
Yooshyasha <mejzikrejzi66@gmail.com>
2025-01-27 23:07:18 +0500

    Initial Commit

---
+++ src/main/kotlin/com/yooshyasha/mvpbussinescrm/MvpBussinesCrmApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.mvpbussinescrm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvpBussinesCrmApplication
+
+fun main(args: Array<String>) {
+    runApplication<MvpBussinesCrmApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/mvpbussinescrm/MvpBussinesCrmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.mvpbussinescrm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MvpBussinesCrmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
